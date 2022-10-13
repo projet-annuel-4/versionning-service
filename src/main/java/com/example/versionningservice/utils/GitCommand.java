7a752b7279db3809d2d0f16ed8ec706850df4ed9
Ennasiri Mehdi <ennasirimehdi@gmail.com>
2022-10-14 00:08:53 +0200

    change git git cmd

--- src/main/java/com/example/versionningservice/utils/GitCommand.java
+++ src/main/java/com/example/versionningservice/utils/GitCommand.java
@@ -18,14 +18,14 @@ public final class GitCommand {
     public final static String REVERT_COMMIT_CONTINUE = "git -C %s revert --continue --no-commit";
     public final static String REVERT_COMMIT_ABORT = "git -C %s revert --abort";
 
-    public final static String MERGE = "git -C \"%s\" merge --no-commit \"%s\"";
-    public final static String MERGE_ABORT = "git -C \"%s\" merge --abort";
-    public final static String MERGE_CONTINUE = "git -C \"%s\" merge \"%s\"";
-    public final static String CREATE_BRANCH = "git -C \"%s\" checkout -b \"%s\"";
-    public final static String ACTUAL_BRANCH = "git -C \"%s\" branch --show-current";
-    public final static String DELETE_BRANCH = "git -C \"%s\" branch -D \"%s\"";
-    public final static String CHECKOUT = "git -C \"%s\" checkout \"%s\"";
-    public final static String LIST_BRANCHES = "git -C \"%s\" branch";
-    public final static String LIST_FILES = "git -C \"%s\" ls-files";
+    public final static String MERGE = "git -C %s merge --no-commit \"%s\"";
+    public final static String MERGE_ABORT = "git -C %s merge --abort";
+    public final static String MERGE_CONTINUE = "git -C %s merge \"%s\"";
+    public final static String CREATE_BRANCH = "git -C %s checkout -b \"%s\"";
+    public final static String ACTUAL_BRANCH = "git -C %s branch --show-current";
+    public final static String DELETE_BRANCH = "git -C %s branch -D \"%s\"";
+    public final static String CHECKOUT = "git -C %s checkout \"%s\"";
+    public final static String LIST_BRANCHES = "git -C %s branch";
+    public final static String LIST_FILES = "git -C %s ls-files";
 
 }
